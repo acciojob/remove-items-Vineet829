16e9d51fe69c0a012a9a8fa0412ac5d325230107
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-29 16:18:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+function removeColor() {
+    document.querySelector("input[type='button']").addEventListener("click", () => {
+    let element = document.querySelector("#colorSelect")
+		let item = element.value
+		let arr = Array.from(element.children)
+		let arr1 = []
+		for(let i of arr){
+			arr1.push(i.value)
+		}
+	arr = arr1.filter((i) => i !== item)
+	  element.innerHTML = ""
+		arr.forEach((ele) => {
+        element.innerHTML += `<option>${ele}</option>` 
+		
+	})
+	})
+	
+}
+
+removeColor()
